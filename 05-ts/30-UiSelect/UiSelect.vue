36e328a6ef8b60f7740a1b2c28a3ff24e0451ca9
Alexandra <alexa_4343@mail.ru>
2025-01-19 17:12:56 +0300

    Решение задачи UiSelect

--- 05-ts/30-UiSelect/UiSelect.vue
+++ 05-ts/30-UiSelect/UiSelect.vue
@@ -1,9 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts" generic="T extends string">
+
+type Props = {
+  options: {
+    text: string,
+    value: 'T'
+  }[]
+}
+
+defineProps<Props>()
+
+const model = defineModel<T>()
+
+</script>
 
 <template>
-  <select class="select">
-    <option value="value-1">One</option>
-    <option value="value-2">Two</option>
+  <select class="select" v-model="model">
+    <option v-for='option in options' 
+      :key="option.value" 
+      :value="option.value"
+    >{{ option.text }}</option>
   </select>
 </template>
 
